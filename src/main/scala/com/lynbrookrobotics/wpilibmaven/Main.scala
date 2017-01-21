19eefe30cfe93f60e39acc26bffd1274ab2a2f59
Sang Hak (Andy) Chun <sanghak@andychun.info>
2017-01-20 19:04:20 -0800

    Add CTRLib as a Maven artifact

--- src/main/scala/com/lynbrookrobotics/wpilibmaven/Main.scala
+++ src/main/scala/com/lynbrookrobotics/wpilibmaven/Main.scala
@@ -14,33 +14,35 @@ object Main extends App {
     def /(subpath: String) = new File(file, subpath)
   }
 
-  def eclipseURLBase(channel: String) = s"http://first.wpi.edu/FRC/roborio/$channel/eclipse"
-
-  val downloadsDir = new File("wpi-downloads")
-  downloadsDir.mkdir()
-
   val repoDir = new File("repo")
 
+  // WPIlib
+
+  def eclipseURLBase(channel: String) = s"http://first.wpi.edu/FRC/roborio/$channel/eclipse"
+
+  val wpiDownloadsDir = new File("wpi-downloads")
+  wpiDownloadsDir.mkdir()
+
   def getLatestWPIVersion(channel: String): String = {
-    (new URL(s"${eclipseURLBase(channel)}/content.jar") #> (downloadsDir / "content.jar")).!
-    s"unzip -o ${(downloadsDir / "content.jar").getAbsolutePath} -d ${downloadsDir.getAbsolutePath}/content".!
+    (new URL(s"${eclipseURLBase(channel)}/content.jar") #> (wpiDownloadsDir / "content.jar")).!
+    s"unzip -o ${(wpiDownloadsDir / "content.jar").getAbsolutePath} -d ${wpiDownloadsDir.getAbsolutePath}/content".!
 
     val prefix = "<unit id='edu.wpi.first.wpilib.plugins.java' version='"
-    Source.fromFile(downloadsDir / "content" / "content.xml").getLines().
+    Source.fromFile(wpiDownloadsDir / "content" / "content.xml").getLines().
       find(_.trim.startsWith(prefix)).get.trim.drop(prefix.length).dropRight(2).trim
   }
 
   def downloadWPILib(version: String, channel: String) = {
     val pluginJARPath = s"${eclipseURLBase(channel)}/plugins/edu.wpi.first.wpilib.plugins.java_$version.jar"
-    (new URL(pluginJARPath) #> (downloadsDir / "plugin.jar")).!
+    (new URL(pluginJARPath) #> (wpiDownloadsDir / "plugin.jar")).!
 
-    s"unzip -o ${(downloadsDir / "plugin.jar").getAbsolutePath} -d ${downloadsDir.getAbsolutePath}/plugin".!
-    s"unzip -o ${(downloadsDir / "plugin" / "resources" / "java.zip").getAbsolutePath} -d ${downloadsDir.getAbsolutePath}/java".!
+    s"unzip -o ${(wpiDownloadsDir / "plugin.jar").getAbsolutePath} -d ${wpiDownloadsDir.getAbsolutePath}/plugin".!
+    s"unzip -o ${(wpiDownloadsDir / "plugin" / "resources" / "java.zip").getAbsolutePath} -d ${wpiDownloadsDir.getAbsolutePath}/java".!
   }
 
   def installWPILib(version: String): Unit = {
-    val wpilibMain = downloadsDir / "java" / "lib" / "WPILib.jar"
-    val wpilibSources = downloadsDir / "java" / "lib" / "WPILib-sources.jar"
+    val wpilibMain = wpiDownloadsDir / "java" / "lib" / "WPILib.jar"
+    val wpilibSources = wpiDownloadsDir / "java" / "lib" / "WPILib-sources.jar"
 
     (s"mvn deploy:deploy-file " +
       s"-Dfile=${wpilibMain.getAbsolutePath} " +
@@ -50,8 +52,8 @@ object Main extends App {
       s"-Dversion=$version " +
       s"-Dsources=${wpilibSources.getAbsolutePath}").!
 
-    val networktablesMain = downloadsDir / "java" / "lib" / "NetworkTables.jar"
-    val networktablesSources = downloadsDir / "java" / "lib" / "NetworkTables-sources.jar"
+    val networktablesMain = wpiDownloadsDir / "java" / "lib" / "NetworkTables.jar"
+    val networktablesSources = wpiDownloadsDir / "java" / "lib" / "NetworkTables-sources.jar"
 
     (s"mvn deploy:deploy-file " +
       s"-Dfile=${networktablesMain.getAbsolutePath} " +
@@ -61,6 +63,37 @@ object Main extends App {
       s"-Dversion=$version " +
       s"-Dsources=${networktablesSources.getAbsolutePath}").!
   }
+
+  // CTR
+
+  val ctrDownloadsDir = new File("ctr-downloads")
+  ctrDownloadsDir.mkdir()
+
+  val ctrURL = "http://www.ctr-electronics.com/downloads/lib"
+
+  def getLatestCTRVersion = "4.4.1.9" // ¯\_(ツ)_/¯ TODO: write scraper method
+
+  def downloadCTR(version: String) = {
+    val ctrZipPath = s"${ctrURL}/CTRE_FRCLibs_NON-WINDOWS_v${version}.zip"
+    (new URL(ctrZipPath) #> (ctrDownloadsDir / "ctr.zip")).!
+
+    s"unzip -o ${(ctrDownloadsDir / "ctr.zip").getAbsolutePath} -d ${ctrDownloadsDir.getAbsolutePath}/ctr"
+  }
+
+  def installCTR(version: String) = {
+    val ctrLib = ctrDownloadsDir / "ctr" / "java" / "lib" / "CTRLib.jar"
+    val ctrJavadoc = ctrDownloadsDir / "ctr" / "java" / "docs" / "CTRLib-javadoc.jar"
+
+    (s"mvn deploy:deploy-file " +
+      s"-Dfile=${ctrLib.getAbsolutePath} " +
+      s"-Durl=file://${repoDir.getAbsolutePath} " +
+      s"-DgroupId=com.ctre " +
+      s"-DartifactId=ctrlib " +
+      s"-Dversion=$version " +
+      s"-Djavadoc=${ctrJavadoc.getAbsolutePath}").!
+  }
+
+  // General
 
   val latestVersionsHTML = new PrintWriter(new File("repo/index.html"))
 
